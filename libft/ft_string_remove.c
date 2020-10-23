b06522e057f3a3b75963aa4acbd41d183093b86a
François <frcs@francois-pc>
2020-10-23 12:13:46 +0200

    updating libft

--- libft/ft_string_remove.c
+++ libft/ft_string_remove.c
@@ -6,7 +6,7 @@
 /*   By: frdescam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 22:02:43 by frdescam          #+#    #+#             */
-/*   Updated: 2020/10/23 10:25:31 by frdescam         ###   ########.fr       */
+/*   Updated: 2020/10/23 12:09:05 by frdescam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ t_string	*ft_string_remove(t_string *string, int start, int end)
 		return (NULL);
 	ft_strlcpy(out->str, &string->str[start], end - start + 2);
 	out->len = end - start + 1;
-	ft_strlcpy(&string->str[start], &string->str[end + 1], string->len - end);
+	ft_strlcpy(&string->str[start], &string->str[end + 1], string->capacity);
 	string->len -= out->len;
 	return (out);
 }
@@ -29,11 +29,11 @@ t_string	*ft_string_remove(t_string *string, int start, int end)
 **
 **int			main(void)
 **{
-**	t_string *string = ft_string_from("s");
+**	t_string *string = ft_string_from("ls > out");
 **
 **	printf("string->str = %s, string->len = %ld, string->capacity = %ld\n",
 **     string->str, string->len, string->capacity);
-**	t_string *removed = ft_string_remove(string, 0, 0);
+**	t_string *removed = ft_string_remove(string, 3, 8);
 **	printf("removed->str = %s, removed->len = %ld, removed->capacity = %ld\n",
 **     removed->str, removed->len, removed->capacity);
 **	printf("string->str = %s, string->len = %ld, string->capacity = %ld\n",
