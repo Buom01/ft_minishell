f38970bb3217a3e55db1e5dd2e3c678bcd2de553
Fdec <fdec@fdec-pc>
2020-10-28 13:12:03 +0100

    fix norm

--- srcs/parsing/parsing.h
+++ srcs/parsing/parsing.h
@@ -6,7 +6,7 @@
 /*   By: frdescam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/27 14:34:05 by frdescam          #+#    #+#             */
-/*   Updated: 2020/10/24 13:47:46 by frdescam         ###   ########.fr       */
+/*   Updated: 2020/10/28 11:31:12 by frdescam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@ void	exec_line(t_string *line);
 void	exec_pipes(t_string *cmd);
 void	exec_redir(t_string *cmd, int fd_in, int fd_out);
 void	exec_action(t_string *cmd, int fd_in, int fd_out);
+void	ft_string_destroy_wrapper(void *content);
 
 #endif
