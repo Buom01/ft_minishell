e6b3f4243cce5008c3deeb1bbfbb2a0ca8768d82
Bastien Adam <bastien.67880@gmail.com>
2020-11-17 14:33:16 +0100

    Undefined (no arg) case of export react as bash does

--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: badam <badam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/22 15:22:15 by badam             #+#    #+#             */
-/*   Updated: 2020/11/05 22:40:52 by badam            ###   ########.fr       */
+/*   Updated: 2020/11/17 14:07:37 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,24 @@
 
 t_error	builtin_export(size_t argc, char **argv)
 {
+	t_env	*entry;
+
+	if (!argc)
+	{
+		entry = *(env_dictionary());
+		while (entry)
+		{
+			if (!env_isinternal(entry->key))
+				if (ft_printf("declare -x %s=\"%s\"\n", entry->key, entry->value) < 0)
+					return (ERR_PRINTF);
+			entry = entry->next;
+		}
+		return (OK);
+	}
 	while (argc)
 	{
-		ft_printf("Exec: %s\n", *argv);
 		if (env_isvalid_equality(*argv))
 			env_create(*argv);
-		else
-			ft_printf("Synthax: %s\n", *argv);
 		argv++;
 		argc--;
 	}
