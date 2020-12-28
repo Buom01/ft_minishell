fc293903d91313345e80504382b37108ce232d73
Bastien <bastien@nc-ass-vip.sdv.fr>
2020-12-28 01:06:04 +0100

    Exit return value on a char

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: badam <badam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/08 18:19:56 by badam             #+#    #+#             */
-/*   Updated: 2020/11/03 21:24:37 by badam            ###   ########.fr       */
+/*   Updated: 2020/12/02 22:52:27 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ t_error	builtin_exit(size_t argc, char **argv)
 	if (argc > 1)
 		return (ERR_TOOMUCH_ARGS);
 	env_shutdown();
-	exit(argc == 1 ? ft_atoi(*argv) : 0);
+	exit(argc == 1 ? (unsigned char)ft_atoi(*argv) : 0);
 	return (OK);
 }
