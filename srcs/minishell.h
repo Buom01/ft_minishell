d59cc1320f3a9955a0c2cbde387123d3127565b7
Bastien Adam <bastien.67880@gmail.com>
2021-01-14 18:15:45 +0100

    Add newline as temporary fix of printf

--- srcs/minishell.h
+++ srcs/minishell.h
@@ -6,7 +6,7 @@
 /*   By: badam <badam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/15 15:24:32 by badam             #+#    #+#             */
-/*   Updated: 2021/01/14 15:18:31 by frdescam         ###   ########.fr       */
+/*   Updated: 2021/01/14 18:14:01 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@
 # include "libft.h"
 # include "builtins/builtins.h"
 
-# define MSG_PROMPT "This is a prompt please enter your cmd $ "
+# define MSG_PROMPT "This is a prompt please enter your cmd $\n"
 
 typedef enum e_error
 {
